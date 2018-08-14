e43f392cc6a5caf407c0c329d5152f39b1b5b14f
jasdg-git <jasdg@yahoo.com>
2018-08-14 14:46:26 -0600

    commit & push exercise JS 1.2.1

--- app.js
+++ app.js
@@ -41,3 +41,16 @@ console.log('But this just being short of jaundice all the time confused them.')
 */
 
 
+// This line of code sets the variable location to the string New York City
+const location = 'New York City';
+
+// This line of code sets the variable latitude to the number 40.7
+let latitude = 40.7;
+
+// This line of code sets the variable inNorthernHemisphere to true
+let inNorthernHemisphere = true;
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
+
+
