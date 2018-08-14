8c77df6a2658b4f6283221012394e3f25094fbd4
jasdg-git <jasdg@yahoo.com>
2018-08-14 15:03:33 -0600

    commit & push exercise JS 1.3.12

--- app.js
+++ app.js
@@ -178,3 +178,20 @@ let fahrenheit = Math.floor(celsius * (9/5) + 32);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
 
+// Kelvin constant for conversion
+const kelvin = 294;
+
+// celsius is 273 degrees less than Kelvin
+let celsius = kelvin - 273;
+
+// formula to convert Celsius to Fahrenheit rounded down
+let fahrenheit = Math.floor(celsius * (9/5) + 32);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+//formula to convert Celsius to Newton
+let newton = Math.floor(celsius * (33/100));
+
+console.log(`The temperature is ${newton} Newton.`)
+
+
