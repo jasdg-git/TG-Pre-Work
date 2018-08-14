a47cfdb624bd43f976513b7dfdda157ef11c055c
jasdg-git <jasdg@yahoo.com>
2018-08-14 14:48:50 -0600

    commit & push exercise JS 1.2.5

--- app.js
+++ app.js
@@ -72,3 +72,17 @@ let valueless;
 console.log(valueless);
 
 
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+
+// Add and assign to molecule below
+molecule += 16;
+
+// Multiply and assign to particle below
+particle *= 6.02;
+
+// Increment assay by 1
+assay += 1;
+
+
