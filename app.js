575384e07e616e618a5303eab02aa2fcfdd21f25
jasdg-git <jasdg@yahoo.com>
2018-08-15 19:05:03 -0600

    commit & push exercise JS 7.4

--- app.js
+++ app.js
@@ -806,4 +806,17 @@ let smallNumbers = bigNumbers.map(function(bigNumber) {
 });
 
 
+let randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+let smallNumbers = randomNumbers.filter(randomNumber => randomNumber<250);
+
+let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+let longFavoriteWords = favoriteWords.filter(word => word.length > 7);
+
+// Refactor the code above using arrow function syntax
+
+
+
 
