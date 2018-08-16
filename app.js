98171447381da4221354061e2a6328da572f133d
jasdg-git <jasdg@yahoo.com>
2018-08-16 13:37:48 -0600

    commit & push exercise JS 12.1.2

--- app.js
+++ app.js
@@ -2208,4 +2208,12 @@ const changeButton = () => {
 jsonButton.addEventListener('click', generateJson);
 
 
+console.log('First message!');
+setTimeout(() => {
+   console.log('This message will always run last...');
+}, 2500);
+console.log('Second message!');
+
+
+
 
