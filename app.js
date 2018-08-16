f4a0398459c32592580bf5023c9c0dc2709db948
jasdg-git <jasdg@yahoo.com>
2018-08-15 19:04:00 -0600

    commit & push exercise JS 7.2

--- app.js
+++ app.js
@@ -783,4 +783,12 @@ console.log(onlyNumbers);
 
 
 
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach( fruit => 
+  console.log('I want to eat a '+fruit)
+);
+
+
 
