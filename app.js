cf863aee59e43d703d3e798f91d95950f44846b7
jasdg-git <jasdg@yahoo.com>
2018-08-15 18:08:38 -0600

    commit & push exercise JS 2.4

--- app.js
+++ app.js
@@ -235,3 +235,21 @@ if (!favoritePhrase) {
 }
 
 
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = 'a';
+
+if (!favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+
