2ad044d4b14374d501585c10016927a12f5cc8df
jasdg-git <jasdg@yahoo.com>
2018-08-15 18:09:45 -0600

    commit & push exercise JS 2.6

--- app.js
+++ app.js
@@ -261,3 +261,26 @@ if (hungerLevel>7) {
 }
 
 
+let moonPhase = 'full';
+
+switch (moonPhase) {
+  case 'full': console.log('Howl!'); break;
+  case 'mostly full': console.log('Arms and legs are getting hairier'); break;
+  case 'mostly new': console.log('Back on two feet'); break;
+  default: console.log('Invalid moon phase'); break;
+}
+
+/*
+if (moonPhase === 'full' || isFoggyNight) {
+  console.log('Howl!');
+} else if (moonPhase === 'mostly full') {
+  console.log('Arms and legs are getting hairier.');
+} else if (moonPhase === 'mostly new') {
+  console.log('Back on two feet');
+}
+  else {
+  console.log('Invalid moon phase.');
+}
+*/
+
+
