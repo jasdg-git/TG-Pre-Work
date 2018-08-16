f0bfc6bb16378fbbf7601797f553f38bd85aa32f
jasdg-git <jasdg@yahoo.com>
2018-08-15 19:28:59 -0600

    commit & push exercise JS 9.7

--- app.js
+++ app.js
@@ -1430,4 +1430,24 @@ surgeonCurry.takeVacationDays(3);
 console.log(surgeonCurry.remainingVacationDays);
 
 
+class HospitalEmployee {
+  constructor (name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name () {
+    return this._name;
+  }
+  
+  get remainingVacationDays () {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays (daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+
 
