a5b609d1b00c5c559d00519f90444884b157bd52
jasdg-git <jasdg@yahoo.com>
2018-08-15 18:42:50 -0600

    commit & push exercise JS 3.8

--- app.js
+++ app.js
@@ -496,5 +496,16 @@ function isGreaterThan (numberOne, numberTwo) {
 isGreaterThan(3,2);
 
 
+const isGreaterThan = (numberOne, numberTwo) => {
+  if(numberOne > numberTwo){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isGreaterThan(4, 8)
+
+
 
 
