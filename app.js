ed79b267d2f544bf33a3a9da0d4b08235d49d9c8
jasdg-git <jasdg@yahoo.com>
2018-08-15 18:52:23 -0600

    commit & push exercise JS 5.6

--- app.js
+++ app.js
@@ -652,4 +652,17 @@ console.log(newYearsResolutions.pop());
 
 
 
+let newYearsResolutions = ['diet', 'have fun', 'be free'];
+console.log (newYearsResolutions);
+let listItem = newYearsResolutions[0];
+console.log (listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
+newYearsResolutions[1] = 'Learn a new language';
+console.log(newYearsResolutions.length);
+newYearsResolutions.push('read', 'run');
+console.log(newYearsResolutions);
+console.log(newYearsResolutions.pop());
+
+
 
