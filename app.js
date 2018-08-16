98c0fa6bbe135afddcf713aa6cf88c94c1d24d6f
jasdg-git <jasdg@yahoo.com>
2018-08-16 13:57:07 -0600

    commit & push exercise JS 12.1.9

--- app.js
+++ app.js
@@ -2308,4 +2308,31 @@ xhr.open('POST', url);
 xhr.send(data);
 
 
+// Information to reach API
+const apiKey = '<Your API Key>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+}
+
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+
+
 
