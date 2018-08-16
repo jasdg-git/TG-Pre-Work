cae088473fc0d808324dd630064849be5bed92bf
jasdg-git <jasdg@yahoo.com>
2018-08-15 18:55:44 -0600

    commit & push exercise JS 6.1

--- app.js
+++ app.js
@@ -694,4 +694,11 @@ console.log(groceryList);
 console.log(groceryList.slice(1,4));
 
 
+let cookies = ['chocolate chip', 'raisin', 'macadamia nut', 'sugar'];
+
+for (let i = 0; i<cookies.length; i++) {
+  console.log('I would love to eat a ' + cookies[i] + ' cookie right now!');
+}
+
+
 
