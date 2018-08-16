c2de8e4496e14d1858a5915aa0c8d9bf3a47605a
jasdg-git <jasdg@yahoo.com>
2018-08-15 18:06:53 -0600

    commit & push exercise JS 2.1

--- app.js
+++ app.js
@@ -195,3 +195,17 @@ let newton = Math.floor(celsius * (33/100));
 console.log(`The temperature is ${newton} Newton.`)
 
 
+let userName = 'jason';
+let knowsJavaScript = true;
+
+if (knowsJavaScript && userName) {
+  console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
+} else if (knowsJavaScript) {
+  console.log('Great! Get ready to practice your JavaScript!');
+} else if (userName) {
+  console.log('Great, ' + userName + '! Get ready to learn something new!');
+} else {
+  console.log('Great! Get ready to learn something new!');
+}
+
+
