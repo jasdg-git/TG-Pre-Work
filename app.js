5684bf1b69c38134998c1245ae89251a6f50a5af
jasdg-git <jasdg@yahoo.com>
2018-08-16 15:01:40 -0600

    commit & push exercise 12.2.12

--- app.js
+++ app.js
@@ -2629,3 +2629,16 @@ shortenButton.addEventListener('click', displayShortUrl);
 
 
 
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint');
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  }
+  catch(error) {console.log(error)};
+};
+
+
