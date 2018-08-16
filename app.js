305cc99aee6abc3298b1c5fbdb1be5a5ebde081b
jasdg-git <jasdg@yahoo.com>
2018-08-16 12:08:01 -0600

    commit & push exercise JS 10.7

--- app.js
+++ app.js
@@ -1690,4 +1690,14 @@ let sauce = "Eggs and cheese"; // ES6 syntax
 const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
 
 
+var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+
 
