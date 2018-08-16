29b44ea7c9b5a2cbfb2434a4a8fea30e068ea576
jasdg-git <jasdg@yahoo.com>
2018-08-16 13:48:42 -0600

    commit & push exercise JS 12.1.5

--- app.js
+++ app.js
@@ -2228,4 +2228,30 @@ xhr.open('GET', url);
 xhr.send();
 
 
+// Information to reach API
+
+
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+
+}
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  };
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
+
+
+
 
