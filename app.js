e4b1dd2615ce20ae6374b17ef7fb76a85d745fd2
jasdg-git <jasdg@yahoo.com>
2018-08-16 15:02:15 -0600

    commit & push exercise JS 12.2.13

--- app.js
+++ app.js
@@ -2642,3 +2642,29 @@ const getData = async () => {
 };
 
 
+// Information to reach API
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jja=';
+
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+// Code goes here
+
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
+
+
+
