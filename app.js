8612d740496e4cceb6ec320e55ee6d4636cf6e87
jasdg-git <jasdg@yahoo.com>
2018-08-15 19:21:09 -0600

    commit & push exercise JS 8.13

--- app.js
+++ app.js
@@ -1255,4 +1255,26 @@ person.age = 39;
 console.log(person.age);
 
 
+let person = {
+  _name: 'Lu Xun',
+  _age: 137,
+  set age(newAge) {
+    if (typeof newAge === 'number') {
+      this._age = newAge;
+    } else {
+      return 'Invalid input';
+    }
+  },
+  
+  get age() {
+    console.log(`${this._name} is ${this._age} years old.`);
+    return this._age;
+  }
+};
+
+person.age = 'Thirty-nine';
+person.age = 39;
+console.log(person.age);
+
+
 
