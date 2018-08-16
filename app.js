d87714cd0e2b34908687634b1cd0fd127df521e3
jasdg-git <jasdg@yahoo.com>
2018-08-15 19:04:31 -0600

    commit & push exercise JS 7.3

--- app.js
+++ app.js
@@ -791,4 +791,19 @@ fruits.forEach( fruit =>
 );
 
 
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+let secretMessage = animals.map(animal => animal[0]);
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+let smallNumbers = bigNumbers.map(function(bigNumber) {
+  return bigNumber/100;
+});
+
+
 
