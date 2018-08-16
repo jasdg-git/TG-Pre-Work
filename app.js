1a429779b6c730f87b43b0700e3b643c936d1ec0
jasdg-git <jasdg@yahoo.com>
2018-08-16 12:22:40 -0600

    commit & push exercise JS 11.12

--- app.js
+++ app.js
@@ -2051,4 +2051,32 @@ export {availableAirplanes as aircrafts,
 export default meetsSpeedRangeRequirements;
 
 
+ import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+import meetsSpeedRangeRequirements from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(element =>{
+  //console.log('Fuel Capacity of ' +element.name+': '+ element.fuelCapacity);
+    console.log(`Fuel Capacity of ${element.name} : ${element.fuelCapacity}`);
+});
+}
+
+function displayStaffStatus() {
+  availableAirplanes.forEach( function(element)
+  {console.log(element.name+' meets staff requirements: '+meetsStaffRequirements(element.availableStaff,flightRequirements.requiredStaff) );} );
+//{console.log(`${element.name} meets staff requirements: ${meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff)}`);});
+}
+
+function displaySpeedRangeStatus() {
+  availableAirplanes.forEach( function(element){
+    console.log(element.name+' meets speed range requirements: ' +meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+  });
+}
+
+displayFuelCapacity();
+displayStaffStatus();
+displaySpeedRangeStatus();
+
+
 
