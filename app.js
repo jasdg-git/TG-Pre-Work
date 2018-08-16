e3e13cfbd320c16a65834db7c6925f4d984e809d
jasdg-git <jasdg@yahoo.com>
2018-08-16 12:06:27 -0600

    commit & push exercise JS 10.4

--- app.js
+++ app.js
@@ -1660,4 +1660,14 @@ let esFivePercentageSupport = 98.2;
 let esSixTemplateLiterals = 89.62;
 
 
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = 'You can make carbonara with '+pasta+','+ meat +', '+ 'and a sauce made with '+sauce;
+
+
 
