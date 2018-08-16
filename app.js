ea94bb00c3e320cee137f43c9dfd82a302ba921c
jasdg-git <jasdg@yahoo.com>
2018-08-15 18:46:34 -0600

    commit & push exercise JS 4.3

--- app.js
+++ app.js
@@ -528,5 +528,19 @@ console.log(stars);
 
 
 
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const myNightSky = function() { 
+  stars = 'Sirius';
+  return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(myNightSky());
+console.log(stars);
+
+
+
 
 
