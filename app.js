7377abc62dcb7a77213a4a460bbd9245497aa17a
jasdg-git <jasdg@yahoo.com>
2018-08-15 18:13:33 -0600

    commit & push exercise JS 3.1

--- app.js
+++ app.js
@@ -403,4 +403,22 @@ favoritePhrase === 'Love That!' ?
 
   
   
-  
+  let calculatorIsOn = false;
+
+const pressPowerButton = () => {
+  if (calculatorIsOn) {
+    console.log('Calculator turning off.');
+    calculatorIsOn = false;
+  } else {
+    console.log('Calculator turning on.');
+    calculatorIsOn = true;
+  }
+};
+
+pressPowerButton();
+// Output: Calculator turning on.
+
+pressPowerButton();
+// Output: Calculator turning off.
+
+
