229b94e8952ad6e51cee7fb43fc991a7604dead0
jasdg-git <jasdg@yahoo.com>
2018-08-15 18:27:42 -0600

    commit & push exercise JS 3.5

--- app.js
+++ app.js
@@ -442,5 +442,23 @@ takeOrder('pepperoni', 'thick crust');
 takeOrder('sausage', 'pan crust');
 
 
+let orderCount = 0;
+
+const takeOrder = (topping, crustType) => {
+  orderCount++;
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+};
+
+console.log(getSubTotal(orderCount));
+
+
 
 
