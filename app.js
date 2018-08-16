ff40a950fb3cd8397dc20d41b9b7f99037f21834
jasdg-git <jasdg@yahoo.com>
2018-08-15 18:58:26 -0600

    commit & push exercise JS 6.6

--- app.js
+++ app.js
@@ -731,4 +731,13 @@ for (let myPlacesIndex=0; myPlacesIndex<myPlaces.length; myPlacesIndex++) {
 }
 
 
+let cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+let currentCard = 'Heart';
+while (currentCard != 'Spade') {
+  console.log(currentCard);
+  currentCard = cards[Math.floor(Math.random() * 4)];
+}
+console.log("found a spade");
+
+
 
